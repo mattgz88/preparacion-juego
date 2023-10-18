08c521e47d6fde39f91a964bec3c85a6380f9342
TomasCipriano <d47173704@alumnos.itr.edu.ar>
2023-10-17 21:52:28 -0300

    loguica niveles 1-2

--- niveles/lvl1-1.js
+++ niveles/lvl1-1.js
@@ -1,51 +1,90 @@
-let tronco, troncos, pinchos, piedra, salida, mapa;
+let piso, tronco, troncos, pinchos, piedra, power, salida, mapa, water;
 
 function setup() {
     new Canvas(1200, 500);
-    background(200)
+    background(200);
+    world.gravity.y = 20;
+
+    piso = new Sprite();
+	piso.width = 1500;
+	piso.height = 5;
+    piso.x = 200;
+    piso.y = 490;
+    piso.collider = "static";
+
 
     tronco = new Group();
     tronco.w = 40;
     tronco.h = 40;
     tronco.tile = "=";
+    tronco.collider = "static";
 
     troncos = new Group();
     troncos.w = 40;
     troncos.h = 40;
     troncos.tile = "#";
+    troncos.collider = "static";
 
     pinchos = new Group();
     pinchos.w = 40;
     pinchos.h = 10;
     pinchos.tile = "^";
+    pinchos.collider = "static";
 
     piedra = new Group();
     piedra.w = 40;
     piedra.h = 40;
     piedra.tile = "o";
+    piedra.collider = "static";
 
     salida = new Group();
     salida.w = 40;
     salida.h = 40;
     salida.tile = "s";
+    salida.collider = "static";
+
+    power = new Group();
+    power.r = 10;
+    power.tile = "@";
 
     mapa = new Tiles(
         [
             ".................oooo",
-            "...................oo",
+            "..................ooo",
             ".................oooo",
             ".................oooo",
             "...........=.....oooo",
             "...##...=..=........s",
-            "#####^^^=^^=........s"
+            "#####^^^=^^=......@.s",
         ],
         50,
         220,
         41,
         41
     );
+
+    water = new Group();
+    water.color = "blue";
+    water.mass = 3;
+    water.vel.x = -1;
+    water.life = 120;
+
+    //    player.overlaps(pinchos, GameOver);             //player aun no existe
+    //    player.overlaps(salida, NextLevel);             //paris haz tu magia
+    //    player.overlaps(power, transformar);            //eliminar power y convertir a elemento aire
+	water.collides(piso, adios);
 }
 
-function draw () {
-    
+function draw() {
+    clear();
+    new water.Sprite(750, 270, 10);
+}
+
+function adios(water, piso) {
+	water.remove();
+}
+
+function GameOver(player, pinchos) {
+    player.remove();
+
 }
--- niveles/lvl1-2.js
+++ niveles/lvl1-2.js
@@ -1,42 +1,50 @@
-
+let piso;
 
 function setup() {
     new Canvas(1400, 500);
-    background(200)
+    background(200);
+
+    piso = new Sprite();
+    piso.width = 1500;
+    piso.height = 5;
+    piso.x = 400;
+    piso.y = 490;
+    piso.collider = "static";
 
     ramas = new Group();
     ramas.w = 40;
     ramas.h = 40;
     ramas.tile = "*";
+    ramas.collider = "static";
 
     tronco = new Group();
     tronco.w = 40;
     tronco.h = 40;
     tronco.tile = "#";
+    tronco.collider = "static";
 
     piedra = new Group();
     piedra.w = 40;
     piedra.h = 40;
     piedra.tile = "o";
+    piedra.collider = "static";
 
     boton = new Group();
     boton.w = 40;
     boton.h = 20;
     boton.tile = "-";
+    boton.collider = "static";
 
     roca = new Group();
     roca.w = 40;
     roca.h = 40;
     roca.tile = "+";
 
-    llave = new Group();
-    llave.r = 10;
-    llave.tile = "l";
-
     salida = new Group();
     salida.w = 40;
     salida.h = 40;
     salida.tile = "s";
+    salida.collider = "static";
 
     mapa = new Tiles(
         [
@@ -46,15 +54,31 @@ function setup() {
             "oooooooo.........oooooo",
             "ooooooo..........#...oo",
             "......*..........#....s",
-            "......*........-.#.l..s"
+            "......*........-.#....s",
         ],
         50,
         220,
         41,
         41
     );
+
+    roca.overlaps(boton, open);
+    //    fireball.overlaps(ramas, quemar);               //ataque de fuego
+    //    player.overlaps(salida, NextLevel);             //paris haz tu magia
+
 }
 
-function draw () {
-    
+function draw() {}
+
+function quemar(ramas, fireball) {
+    //cambiar textura
+    setTimeout(abrir, 4000);
+}
+function abrir(ramas) {
+    ramas.remove();
+}
+
+function open(roca, boton, tronco) {
+    roca.x = boton.x;
+    player.remove();
 }
